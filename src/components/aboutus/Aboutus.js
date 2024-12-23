e7b0864299b55829fe8e22900e0cbef5fb8d95ef
iam-navyaK <keerthanakoya10@gmail.com>
2024-12-23 17:37:54 +0530

    first version

---
+++ src/components/aboutus/Aboutus.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Aboutus.css'
+
+function Aboutus() {
+  return (
+    <div className='text-center'>
+      <p className="display-2">Aboutus Page</p>
+      <p>Test data</p>
+      <p className="lead">
+      Welcome to USER-MANAGEMENT-APP This is my first project, designed to make user management simple and efficient. It helps with user registration, profile updates, and secure data handling.
+
+This project is a reflection of my learning journey, and I’m excited to grow and improve it over time.
+
+Thank you for visiting and supporting my work!
+      </p>
+    </div>
+  )
+}
+
+export default Aboutus
---
+++ src/components/home/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="text-center">
+      <p className="display-2">Home Page</p>
+      <p className="lead">
+      At USER-MANAGEMENT-APP, we make user management simple, secure, and efficient. Whether you're managing user registrations, profiles, or secure data, our intuitive platform ensures a seamless experience. With easy navigation and powerful features, you can streamline your workflow and focus on what matters most—your users. Get started today and take control of your user management with ease.
+
+Thank you for visiting!
+
+
+
+
+      </p>
+    </div>
+  );
+}
+
+export default Home;
